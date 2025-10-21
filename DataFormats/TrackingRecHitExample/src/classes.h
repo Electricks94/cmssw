460c117918eaeca4d4ab7e13908ced093311fa84
markus <markus.holzer@cern.ch>
2025-10-21 11:15:03 +0200

    Added MultiView

---
+++ DataFormats/TrackingRecHitExample/src/classes.h
@@ -0,0 +1,9 @@
+#ifndef DataFormats_TrackingRecHitExample_src_classes_h
+#define DataFormats_TrackingRecHitExample_src_classes_h
+
+#include "DataFormats/TrackingRecHitExample/interface/TrackingRecHitsSoA.h"
+#include "DataFormats/TrackingRecHitExample/interface/TrackingRecHitsHost.h"
+
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#endif  // DataFormats_TrackingRecHitExample_src_classes_h
